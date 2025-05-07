9500698a3acd0beaffe32389d54bb7b743d8ab53
krb-2005 <ruben.penlaz@educa.jcyl.es>
2025-05-07 13:19:28 +0200

    Borrado MAIN, guardado PRUEBA

---
+++ Prueba.java
@@ -0,0 +1,10 @@
+public class Prueba {
+    private int cantidad;
+    public static void hola() {
+        System.out.println("HolaMundo");
+    }
+
+    public static void main(String[] args) {
+        hola();
+    }
+}
